523534337356dd8819c7a2be3d5b8a1f72d23f80
Kirill Borisov <lensvol@gmail.com>
2020-01-19 21:52:57 +0100

    Add option to run only selected modifications.

--- pybetter/cli.py
+++ pybetter/cli.py
@@ -7,7 +7,7 @@ from pyemojify import emojify
 from pybetter.improvements import *
 from pybetter.utils import resolve_paths
 
-IMPROVEMENTS: List[BaseImprovement] = [
+ALL_IMPROVEMENTS: List[BaseImprovement] = [
     FixNotInConditionOrder(),
     FixMutableDefaultArgs(),
     FixParenthesesInReturn(),
@@ -18,11 +18,11 @@ IMPROVEMENTS: List[BaseImprovement] = [
 ]
 
 
-def process_file(source: str) -> str:
+def process_file(source: str, improvements: List[BaseImprovement]) -> str:
     tree: cst.Module = cst.parse_module(source)
     modified_tree: cst.Module = tree
 
-    for case in IMPROVEMENTS:
+    for case in improvements:
         intermediate_tree = modified_tree
         modified_tree = case.improve(intermediate_tree)
 
@@ -51,20 +51,47 @@ def cli():
     default=False,
     help="Show diff-like output of the changes made.",
 )
+@click.option(
+    "--select",
+    "selected",
+    type=str,
+    metavar="CODES",
+    help="Apply only improvements with the provided codes.",
+)
 @click.argument("paths", type=click.Path(), nargs=-1)
-def main(paths, noop: bool, show_diff: bool):
+def main(paths, noop: bool, show_diff: bool, selected: str):
     if not paths:
         print(emojify("Nothing to do. :sleeping:"))
         return
 
     python_files = filter(lambda fn: fn.endswith(".py"), resolve_paths(*paths))
 
+    selected_improvements = ALL_IMPROVEMENTS
+    if selected:
+        all_codes = frozenset([improvement.CODE for improvement in ALL_IMPROVEMENTS])
+        selected_codes = frozenset(map(str.strip, selected.split(",")))
+
+        wrong_codes = selected_codes.difference(all_codes)
+        if wrong_codes:
+            print(
+                emojify(
+                    f":no_entry_sign: Unknown improvements selected: {','.join(wrong_codes)}"
+                )
+            )
+            return
+
+        selected_improvements = [
+            improvement
+            for improvement in ALL_IMPROVEMENTS
+            if improvement.CODE in selected_codes
+        ]
+
     for path_to_source in python_files:
-        with open(path_to_source, 'r+') as source_file:
+        with open(path_to_source, "r+") as source_file:
             print(f"--> Processing '{source_file.name}'...")
 
             original_source: str = source_file.read()
-            processed_source: str = process_file(original_source)
+            processed_source: str = process_file(original_source, selected_improvements)
 
             if original_source == processed_source:
                 print("  Nothing changed.")
