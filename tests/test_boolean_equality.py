d6b97d05885b925bbf13d1eaabaf33ff7ced7152
Kirill Borisov <lensvol@gmail.com>
2020-01-27 21:07:44 +0100

    Add some tests for 'boolean_equality'.

---
+++ tests/test_boolean_equality.py
@@ -0,0 +1,19 @@
+import pytest
+
+from pybetter.cli import process_file
+from pybetter.improvements import FixBooleanEqualityChecks
+
+
+@pytest.mark.parametrize(
+    "original,expected",
+    [
+        ("a == False", "not a"),
+        ("a == True", "a"),
+        ("a == b < 42 == True", "a == b < 42"),
+        ("(a == True) == False", "not (a == True)"),
+    ],
+)
+def test_boolean_equality_fix(original, expected):
+    processed = process_file(original, [FixBooleanEqualityChecks()])
+
+    assert processed == expected
