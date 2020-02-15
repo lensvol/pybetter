c598f3b64ff6b416f50bc8f1fca7b57b165ce94e
Kirill Borisov <lensvol@gmail.com>
2020-02-15 16:27:31 +0100

    Add noqa to silence flake8 about formatting introduced by black.

--- pybetter/transformers/unhashable_list.py
+++ pybetter/transformers/unhashable_list.py
@@ -33,7 +33,7 @@ class UnhashableListTransformer(NoqaAwareTransformer):
             func=m.OneOf(m.Name(value="set"), m.Name(value="frozenset")),
             args=[m.Arg(value=m.OneOf(m.List(), m.Tuple(), m.Set()))],
         )
-        | m.Set()
+        | m.Set()  # noqa: W503
     )
     @m.leave(m.List() | m.Set() | m.Tuple())
     def convert_list_arg(
