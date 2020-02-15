4bb83d9e15dc887edb7a1275fcea7852048f924d
Kirill Borisov <lensvol@gmail.com>
2020-02-15 15:42:15 +0100

    equals_none: Added tests.

---
+++ tests/test_equals_none.py
@@ -0,0 +1,48 @@
+import pytest
+
+from pybetter.cli import process_file
+from pybetter.improvements import FixEqualsNone
+
+NO_CHANGES_MADE = None
+
+TRIVIAL_NONE_COMPARISON = (
+    """
+    if a == None:
+        pass 
+    """,
+    """
+    if a is None:
+        pass
+    """,
+)
+
+
+NONE_IDENTITY_CHECK_IGNORED = (
+    """
+    if a is None:
+        pass
+    """,
+    NO_CHANGES_MADE,
+)
+
+
+NESTED_NONE_CMPS_PROCESSED = (
+    """
+    if a == None == None == None:
+        pass
+    """,
+    """
+    if a is None is None is None:
+        pass
+    """,
+)
+
+
+@pytest.mark.parametrize(
+    "original,expected",
+    [TRIVIAL_NONE_COMPARISON, NONE_IDENTITY_CHECK_IGNORED, NESTED_NONE_CMPS_PROCESSED],
+)
+def test_trivial_fmt_string_conversion(original, expected):
+    processed, _ = process_file(original.strip(), [FixEqualsNone()])
+
+    assert processed.strip() == (expected or original).strip()
