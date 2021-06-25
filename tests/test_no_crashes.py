462cfcf068abbddf0b4a43014e368e668d5ce86a
Kirill Borisov <lensvol@gmail.com>
2021-06-25 21:06:28 +0200

    Use Hypothesmith for checking resilience against random code.

---
+++ tests/test_no_crashes.py
@@ -0,0 +1,18 @@
+import hypothesmith
+import pytest
+from hypothesis import given, settings, HealthCheck
+
+from pybetter.cli import process_file, ALL_IMPROVEMENTS
+
+
+settings.register_profile(
+    "slow_example_generation",
+    suppress_health_check=HealthCheck.all(),
+)
+settings.load_profile("slow_example_generation")
+
+
+@pytest.mark.slow
+@given(generated_source=hypothesmith.from_grammar())
+def test_no_crashes_on_valid_input(generated_source):
+    process_file(generated_source, ALL_IMPROVEMENTS)
